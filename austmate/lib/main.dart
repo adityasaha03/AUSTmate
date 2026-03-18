2707b37c5442276634a3858fac947f4bb4c923e2
Aditya Saha <77659979+adityasaha03@users.noreply.github.com>
2026-03-18 15:16:22 +0600

    test update

--- austmate/lib/main.dart
+++ austmate/lib/main.dart
@@ -7,6 +7,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'pages/library_page.dart';
 import 'pages/profile_page.dart';
 
+// test update
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
